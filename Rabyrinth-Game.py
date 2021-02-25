04d8c026b35603a67d4dbb0e185bc703156184fe
channarypha <channary.pha@student.passerellesnumeriques.org>
2021-02-25 23:45:56 +0700

    2

--- Rabyrinth-Game.py
+++ Rabyrinth-Game.py
@@ -2,13 +2,12 @@
 import tkinter as tk
 
 # CONSTANTS
-SCREEN_WIDTH = 900
-SCREEN_HEIGHT = 900
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
 X = -1
 Y = -1
 # VARIABLES
-grid = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
-
+grid = [[0,0,0,0,0,0,0,2,0,5,1,1,1,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],[0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],[0,0,1,1,1,1,1,1,1,1,1,1,1,2,0,1,1,1,0],[0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],[0,0,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0],[0,0,1,0,2,0,0,0,0,0,0,0,0,1,0,0,0,0,0],[3,1,1,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0],[0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],[0,0,1,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0],[2,1,1,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0],[0,0,2,1,1,1,0,0,0,0,2,1,1,1,0,0,0,0,0],[0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 indexOfone = 0
 # FUNCTION
 def getIndex1():
@@ -23,16 +22,21 @@ def getIndex1():
 
 def arrayToDrawing():
     for i in range(len(grid)):
-        y1 = 50 * i
-        y2 = 50+ y1
+        y1 = 40 * i
+        y2 = 40+ y1
         for j in range(len(grid[0])):
-            x1 = j*50
-            x2 = x1 + 50
-            if grid[i][j] == 0:
-                color = "pink"
-            else:
-                color = "black"
-            canvas.create_oval(x1, y1, x2, y2, fill=color)
+            x1 = j*40
+            x2 = x1 + 40
+            # if grid[i][j] == 0:
+            #     canvas.create_rectangle(x1, y1, x2, y2, fill = "pink")
+            if grid[i][j] == 1:
+                canvas.create_oval(x1, y1, x2, y2, fill = "gray")  
+            elif grid[i][j] ==  3 :
+                canvas.create_oval(x1, y1, x2, y2, fill = "black") 
+            elif grid[i][j] == 2 :
+                canvas.create_oval(x1, y1, x2, y2, fill = "red")
+            elif grid[i][j] == 5 :
+                canvas.create_oval(x1, y1, x2, y2, fill = "green")
 
 # draw a line with white and black squares using the global array
 def leftclick(event):
@@ -40,7 +44,7 @@ def leftclick(event):
     getIndex1()
     if X :
         grid[Y][X] = 0
-        grid[Y][X-1] = 1
+        grid[Y][X-1] = 3
 
     
     arrayToDrawing()
@@ -52,9 +56,9 @@ def leftclick(event):
 def rightclick(event):
     global grid
     getIndex1()
-    if X<len(grid[0])-1:
+    if X<len(grid[0]) - 1:
         grid[Y][X] = 0
-        grid[Y][X+1] = 1
+        grid[Y][X+1] = 3
     print(grid)
     
     arrayToDrawing()
@@ -63,7 +67,7 @@ def upclick(event):
     getIndex1() 
     if Y :
             grid[Y][X] = 0
-            grid[Y-1][X] = 1
+            grid[Y-1][X] = 3
     print(grid)
     arrayToDrawing()
 def downclick(event):
@@ -71,17 +75,23 @@ def downclick(event):
     getIndex1()
     if Y <len(grid)-1:
             grid[Y][X] = 0
-            grid[Y+1][X] = 1
+            grid[Y+1][X] = 3
     print(grid)
     arrayToDrawing()
 root = tk.Tk()
-root.bind("<Left>", leftclick) #LEFT CLICK
-root.bind("<Right>", rightclick)  #RIGHT CLICK
-root.bind("<Up>", upclick)  #UP CLICK
-root.bind("<Down>", downclick)  #DOWN CLICK
-root.geometry(str(SCREEN_WIDTH)+"x"+str(SCREEN_HEIGHT))
 
-canvas = tk.Canvas(root)
+#LEFT CLICK
+root.bind ( " <Left> ", leftclick) 
+#RIGHT CLICK
+root.bind ( " <Right> ", rightclick) 
+#UP CLICK
+root.bind ( " <Up> ", upclick) 
+#DOWN CLICK 
+root.bind ( " <Down> ", downclick)  
+
+root.geometry ( str (SCREEN_WIDTH) + "x" + str ( SCREEN_HEIGHT ) )
+
+canvas = tk.Canvas (root)
 canvas.pack(expand=True, fill="both")
 arrayToDrawing()
 
